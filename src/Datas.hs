65a804d945a86b8001ca0a5c9ec6f61e053b240d
Zachary Gurwitz <66977988+zgurwitz@users.noreply.github.com>
2022-05-14 23:42:29 -0400

    Update Datas.hs

--- src/Datas.hs
+++ src/Datas.hs
@@ -1,5 +1,8 @@
 module Datas where
 import qualified Data.Map as Map
+import Control.Lens hiding ((<|), (|>))
+import Control.Lens.TH
+
 type Loc = (Int, Int)
 type Stage = Map.Map Loc Object
 
@@ -11,3 +14,30 @@ data Direction = North | South | East | West | Stay
 
 data Item = Bullet | Life | Speed | Teleport
   deriving (Eq, Show, Ord)
+
+data MobileEntity = IsPlayer | IsEnemy | IsProjectile
+
+canMoveHere :: Stage -> Loc -> MobileEntity -> Bool
+canMoveHere s l me = case (getAtStage l s , me) of
+  (Just Player , _ )-> True
+  (Just Enemy , IsEnemy) -> False
+  (Just Enemy , _) -> True
+  (Just Obstacle , _) -> False
+  (Just Goal , IsEnemy) -> False
+  (Just Goal , _) -> True
+  (Just Vacant , _) -> True
+  (Just Boom, _) -> True
+  (Just (Projectile _), _) -> True
+  (Nothing , _)-> False
+
+
+getAtStage :: Loc -> Stage -> Maybe Object
+getAtStage l s = s ^. (at l)
+
+nudge :: Loc -> Direction -> Loc
+nudge (x,y) d = case d of
+  Stay -> (x,y)
+  North -> (x, y-1)
+  South -> (x, y+1)
+  East -> (x+1,y)
+  West -> (x-1,y)
